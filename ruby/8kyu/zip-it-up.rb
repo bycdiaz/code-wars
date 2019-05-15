cac215b450e230caae394f87e1c7affb4bd2dc21
bycdiaz <cdiaz86@gmail.com>
2019-05-14 20:44:37 -0400

    Solved zip it up kata & refactored.

---
+++ ruby/8kyu/zip-it-up.rb
@@ -0,0 +1,17 @@
+# Create a method zip that accepts two lists of the same length, and combines their result into a single array, like so:
+
+
+#     #=> ['a', 'b', 'c', 'd', 'e', 'f']
+
+# initial solution
+# def zip first, second
+#   new_list = first + second
+#   new_list.sort
+# end
+
+# refactored
+def zip (first, second) (first + second).sort end
+
+first = ['a', 'c', 'e']
+second = ['b', 'd', 'f']
+p zip(first, second)
