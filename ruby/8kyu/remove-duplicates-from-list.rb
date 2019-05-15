3ef1e8b3c1eb1e6ffdafc3ac1b7ebc361d8fdce0
bycdiaz <cdiaz86@gmail.com>
2019-05-15 09:53:34 -0700

    Adds solutions to opposite number and remove duplicate katas.

---
+++ ruby/8kyu/remove-duplicates-from-list.rb
@@ -0,0 +1,7 @@
+# Define a function that removes duplicates from an array of numbers and returns it as a result.
+
+# The order of the sequence has to stay the same.
+
+def distinct(seq)
+  seq.uniq
+end
