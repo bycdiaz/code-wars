7d52a229ed214acbf7e4c8205e6839098458f3a8
bycdiaz <cdiaz86@gmail.com>
2019-05-16 20:41:35 -0400

    Adds solution to Do I Get A Bonus kata.

---
+++ ruby/8kyu/do-i-get-a-bonus.rb
@@ -0,0 +1,29 @@
+# It's bonus time in the big city! The fatcats are rubbing their 
+# paws in anticipation... but who is going to make the most money?
+
+# Build a function that takes in two arguments (salary, bonus). 
+# Salary will be an integer, and bonus a boolean.
+
+# If bonus is true, the salary should be multiplied by 10. If bonus 
+# is false, the fatcat did not make enough money and must receive 
+# only his stated salary.
+
+# Return the total figure the individual will receive as a string 
+# prefixed with "£" (= "\u00A3", JS and Java) or "$" (C#, C++, Ruby, 
+#   Clojure, Elixir, PHP and Python, Haskell).
+
+# original solution
+# def bonus_time(salary, bonus)
+#   if bonus
+#     "$#{(salary * 10)}"
+#   else
+#     "$#{salary}"
+#   end
+# end
+
+# refactored
+def bonus_time(salary, bonus)
+  bonus ? "$#{(salary * 10)}" : "$#{salary}"
+end
+
+puts bonus_time(2, true)
