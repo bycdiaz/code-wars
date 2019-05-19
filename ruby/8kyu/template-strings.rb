a22565bbd1a291e21aeb347d2de4d2fdd3194a0d
bycdiaz <cdiaz86@gmail.com>
2019-05-19 19:16:31 -0400

    Adds solution to Template Strings kata.

---
+++ ruby/8kyu/template-strings.rb
@@ -0,0 +1,11 @@
+# Template Strings, this kata is mainly aimed at the new
+# JS ES6 Update introducing Template Strings
+# Task
+# Your task is to return the correct string using the 
+# Template String Feature.
+# Input
+# Two Strings, no validation is needed.
+
+def TempleStrings(obj, feature)
+  "#{obj} are #{feature}"
+end
