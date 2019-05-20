ea16c478e740c9b72d5f0f8b8b392598fa7283e7
bycdiaz <cdiaz86@gmail.com>
2019-05-20 10:59:28 -0700

    Adds solution to Remove Duplicate Words kata.

---
+++ ruby/7kyu/remove-duplicate-words.rb
@@ -0,0 +1,19 @@
+# Your task is to remove all duplicate words from a string, 
+# leaving only single (first) words entries.
+
+# Example:
+
+# Input:
+
+# 'alpha beta beta gamma gamma gamma delta alpha beta beta 
+# gamma gamma gamma delta'
+
+# Output:
+
+# 'alpha beta gamma delta'
+
+
+# my initial solution
+def remove_duplicate_words(s)
+  s.split(" ").uniq.join(" ")
+end
