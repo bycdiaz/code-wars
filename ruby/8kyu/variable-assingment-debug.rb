877bd8f60a2cec9c5a5e8c74d26bcf553d908d83
bycdiaz <cdiaz86@gmail.com>
2019-05-22 08:28:11 -0700

    Adds solution to Just Count Sheep and Variable Assignment Debug katas.

---
+++ ruby/8kyu/variable-assingment-debug.rb
@@ -0,0 +1,13 @@
+# Fix the variables assigments so that this code stores 
+# the string 'devLab' in the variable name.
+
+# my initial solution
+# a = "dev"
+# b = "Lab"
+
+# variable assignment refactored
+a,b = "dev","Lab"
+
+name = a + b
+
+puts name
