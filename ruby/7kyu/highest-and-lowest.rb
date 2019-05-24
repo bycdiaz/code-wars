1bcb4e8ab279a052e8fbf5f22d3b3073cb72b744
bycdiaz <cdiaz86@gmail.com>
2019-05-24 12:46:57 -0400

    Adds solution to Highest and Lowest kata.

---
+++ ruby/7kyu/highest-and-lowest.rb
@@ -0,0 +1,25 @@
+# In this little assignment you are given a string of space separated numbers, 
+# and have to return the highest and lowest number.
+
+# Example:
+
+# high_and_low("1 2 3 4 5")  # return "5 1"
+# high_and_low("1 2 -3 4 5") # return "5 -3"
+# high_and_low("1 9 3 4 -5") # return "9 -5"
+# Notes:
+
+# All numbers are valid Int32, no need to validate them.
+# There will always be at least one number in the input string.
+# Output string must be two numbers separated by a single space, and highest 
+# number is first.
+
+def high_and_low(numbers)
+  new_array = []
+  new_array = numbers.split(" ").map{ |num|
+    num.to_i
+  }.sort
+  "#{new_array[-1]} #{new_array[0]}"
+end
+
+p high_and_low("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6")
+# should return "542 -214"
