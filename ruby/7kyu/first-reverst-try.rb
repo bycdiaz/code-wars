2857ad5669ae1790ac5678882eb1834c81b33e3c
bycdiaz <cdiaz86@gmail.com>
2019-05-24 14:09:07 -0400

    Adds solution to First Reverse Try kata.

---
+++ ruby/7kyu/first-reverst-try.rb
@@ -0,0 +1,36 @@
+# Task
+# Reversing an array can be a tough task, especially for a 
+# novice programmer. Mary just started coding, so she would 
+# like to start with something basic at first. Instead of 
+# reversing the array entirely, she wants to swap just its 
+# first and last elements.
+
+# Given an array arr, swap its first and last elements and 
+# return the resulting array.
+
+# Example
+# For arr = [1, 2, 3, 4, 5], the output should be [5, 2, 3, 4, 1]
+
+# Input/Output
+# [input] integer array arr
+
+# Constraints: 0 ≤ arr.length ≤ 50, -1000 ≤ arr[i] ≤ 1000
+
+# [output] an integer array
+
+# Array arr with its first and its last elements swapped.
+
+def first_reverse_try(arr)
+  if arr.empty?
+    []
+  else
+    first = arr[0]
+    last = arr[-1]
+    # arr[0], arr, arr[-1] = first, arr, last
+    arr[0] = last
+    arr[-1] = first
+    arr
+  end
+end
+
+p first_reverse_try([1, 2, 3, 4, 5])
