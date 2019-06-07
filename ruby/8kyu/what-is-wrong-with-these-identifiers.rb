bc4f1d2ebb4b1836c54a5f7ba080124ea1a85b1d
bycdiaz <cdiaz86@gmail.com>
2019-06-06 21:46:21 -0400

    Adds solution to What is Wrong With These Identifiers kata.

---
+++ ruby/8kyu/what-is-wrong-with-these-identifiers.rb
@@ -0,0 +1,8 @@
+# Can you amend this object so that its properties comprise only vaild identifiers?
+
+Person = {
+  "1stname" => "John",
+  "second-name" => "Doe",
+  "email@ddress" => "john.doe@email.com",
+  "male.female" => "M"
+}
