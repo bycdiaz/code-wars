a2368ec6c290275974ff7c3d6cdc322cbf23249b
bycdiaz <cdiaz86@gmail.com>
2019-06-26 20:17:46 -0400

    Adds solution to Barking Mad kata.

---
+++ ruby/8kyu/barking_mad.rb
@@ -0,0 +1,22 @@
+# Teach snoopy and scooby doo how to bark using object methods. Currently only snoopy can bark and not scooby doo.
+
+# snoopy.bark() #return "Woof"
+# scoobydoo.bark() #doesn't work yet
+# Use method prototypes to enable all Dogs to bark.
+
+class Dog
+  def initialize(breed)
+    @breed=breed
+  end
+
+  def bark
+    "Woof"
+  end
+end
+
+snoopy = Dog.new("Beagle")
+
+scoobydoo = Dog.new("Great Dane")
+
+puts snoopy.bark
+puts scoobydoo.bark
