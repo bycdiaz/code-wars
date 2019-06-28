4274dfea6a37afc71a31bfb7cdad2ba06eee8f29
bycdiaz <cdiaz86@gmail.com>
2019-06-28 16:02:32 -0400

    Adds solution to Split that Array kata.

---
+++ ruby/8kyu/Split_that_Array.rb
@@ -0,0 +1,18 @@
+# Create a method partition that accepts a list and a method/block. It should return two arrays: 
+# the first, with all the elements for which the given block returned true, and the second for 
+# the remaining elements.
+
+# Here's a simple Ruby example:
+
+# animals = ["cat", "dog", "duck", "cow", "donkey"]
+# partition(animals){|animal| animal.size == 3}
+#     #=> [["cat", "dog", "cow"], ["duck", "donkey"]]
+# The equivalent in Python would be:
+
+# animals = ['cat', 'dog', 'duck', 'cow', 'donkey']
+# partition(animals, lambda x: len(x) == 3)
+#     # (['cat', 'dog', 'cow'], ['duck', 'donkey'])
+
+def partition list, &block
+  list.partition(&block)
+end
