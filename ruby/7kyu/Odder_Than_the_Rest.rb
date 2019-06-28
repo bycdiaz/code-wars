cd7c0b04a58e534fdbb0f51c9d10b52d747afcc8
bycdiaz <cdiaz86@gmail.com>
2019-06-27 20:48:02 -0400

    Adds solution to Odder Than the Rest kata.

---
+++ ruby/7kyu/Odder_Than_the_Rest.rb
@@ -0,0 +1,20 @@
+# Create a method that takes an array/List as an input, and outputs the index 
+# at which the sole odd number is located.
+
+# This method should work with arrays with negative numbers. If there are no 
+# odd numbers in the array, then the method should output -1.
+
+# my initial solution
+def oddOne(arr)
+  arr.each_with_index { |number,index| 
+    if number.odd?
+      return index
+    end
+    }
+    -1
+end
+
+# oddOne([2,4,6,7,10]) # => 3
+# oddOne([2,16,98,10,13,78]) # => 4
+# oddOne([4,-8,98,-12,-7,90,100]) # => 4
+p oddOne([2,4,6,8]) # => -1
