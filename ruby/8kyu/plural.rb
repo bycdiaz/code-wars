b4b2a7201a8f68eceed9391021c889d69ee15ab4
bycdiaz <cdiaz86@gmail.com>
2019-07-28 14:37:15 -0400

    Adds solution to Plural kata.

---
+++ ruby/8kyu/plural.rb
@@ -0,0 +1,27 @@
+# We need a simple function that determines if a plural is needed or not. It should take a number, 
+# and return true if a plural should be used with that number or false if not. This would be 
+# useful when printing out a string such as 5 minutes, 14 apples, or 1 sun.
+
+# You only need to worry about english grammar rules for this kata, where anything that isn't 
+# singular (one of something), it is plural (not one of something).
+
+# All values will be positive integers or floats, or zero.
+
+def plural(n)
+  n != 1
+end
+
+# puts plural(0)
+# true, "Plural for 0"
+
+# puts plural(0.5)
+# true, "Plural for 0.5"
+
+# puts plural(1)
+# false, "Plural for 1"
+
+# puts plural(100)
+# true, "Plural for 100"
+
+# puts plural(Float::INFINITY)
+# true, "Plural for Infinity"
