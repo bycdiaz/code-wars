5bdcc1cf78ebecb6cc56f734db737e68a41b5543
bycdiaz <cdiaz86@gmail.com>
2019-08-03 22:02:12 -0400

    Adds solution to Leonardo Dicaprio and Oscars kata.

---
+++ ruby/8kyu/Leonardo_Dicaprio_and_Oscars.rb
@@ -0,0 +1,11 @@
+def leo(oscar)
+  if oscar == 88
+    "Leo finally won the oscar! Leo is happy"
+  elsif oscar == 86
+    "Not even for Wolf of wallstreet?!"
+  elsif oscar <= 87
+    "When will you give Leo an Oscar?"
+  else
+    "Leo got one already!"
+  end
+end
