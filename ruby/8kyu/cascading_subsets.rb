20fc7c0fdc3bd4f333a192dbf3497abe57ede60c
bycdiaz <cdiaz86@gmail.com>
2019-08-11 15:53:07 -0400

    Adds solution to Cascading Subsets kata.

---
+++ ruby/8kyu/cascading_subsets.rb
@@ -0,0 +1,17 @@
+# Create a method each_cons that accepts a list and a number n, and returns cascading subsets of the list of size n, like so:
+
+# each_cons([1,2,3,4], 2)
+#   #=> [[1,2], [2,3], [3,4]]
+
+# each_cons([1,2,3,4], 3)
+#   #=> [[1,2,3],[2,3,4]]
+
+# As you can see, the lists are cascading; ie, they overlap, but never out of order.
+
+def each_cons(list, n)
+  cascade = []
+  list.each_cons(n) {|numbers| cascade << numbers }
+  cascade
+end
+
+p each_cons([3, 5, 8, 13],2)
