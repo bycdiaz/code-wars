7ad1a625a8a8dc2e2a9ef65f31ecad0f33075481
Carlos Diaz <cdiaz86@gmail.com>
2019-09-15 14:30:11 -0400

    Adds solution to Grouped by Commas kata.

---
+++ ruby/6kyu/grouped_by_commas.rb
@@ -0,0 +1,34 @@
+# Finish the solution so that it takes an input n (integer) and returns a string that is the
+# decimal representation of the number grouped by commas after every 3 digits.
+
+# Assume: 0 <= n < 2147483647
+# Examples
+
+#        1  ->           "1"
+#       10  ->          "10"
+#      100  ->         "100"
+#     1000  ->       "1,000"
+#    10000  ->      "10,000"
+#   100000  ->     "100,000"
+#  1000000  ->   "1,000,000"
+# 35235235  ->  "35,235,235"
+
+def solution(n)
+  if n.digits.length <= 3
+    n.to_s
+  else
+    array = []
+    n.to_s.split('').reverse.each_slice(3) do |segment|
+      segment.reverse!
+      array << segment
+    end
+    array.map { |segment| segment.join }.reverse.join(",")
+  end
+end
+
+# p solution(1)
+# p solution(10)
+# p solution(100)
+# p solution(1000)
+# p solution(10000)
+p solution(35235235)
