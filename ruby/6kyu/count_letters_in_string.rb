df49e586f4a0bd47d0b61ae7d9fbc8a2eec59db1
Carlos Diaz <cdiaz86@gmail.com>
2019-11-02 12:18:57 -0400

    Reduces code in Count Letters in String kata.

--- ruby/6kyu/count_letters_in_string.rb
+++ ruby/6kyu/count_letters_in_string.rb
@@ -7,20 +7,29 @@
 
 # letterCount('arithmetics') #=> {:a=>1, :c=>1, :e=>1, :h=>1, :i=>2, :m=>1, :r=>1, :s=>1, :t=>2}
 
+# initial solution
+# def letter_count(str)
+#   count = Hash.new 0
+#   str.split("").each do |char|
+#     count[char] += 1
+#   end
+#   result = Hash.new
+#   count.each do |key,value|
+#     result [key.to_sym] = value
+#   end
+#   result.sort.to_h
+# end
+
+# concise-er solution
 def letter_count(str)
   count = Hash.new 0
   str.split("").each do |char|
-    count[char] += 1
+    count[char.to_sym] += 1
   end
-  result = Hash.new
-  count.each do |key,value|
-    result [key.to_sym] = value
-  end
-  result.sort.to_h
+  count.sort.to_h
 end
 
-
-# p letter_count('codewars')
+p letter_count('codewars')
 # {:a=>1, :c=>1, :d=>1, :e=>1, :o=>1, :r=>1, :s=>1, :w=>1}
 
 # p letter_count('activity')
