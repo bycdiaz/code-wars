4c5852eaf6122b47879f47f747496820127eb7f9
Carlos Diaz <cdiaz86@gmail.com>
2019-11-23 13:18:07 -0500

    Adds solution to Power kata.

---
+++ javascript/8kyu/power.js
@@ -0,0 +1,26 @@
+// The goal is to create a function 'numberToPower(number, power)' that "raises" the number up
+// to power (ie multiplies number by itself power times).
+// Examples
+
+// numberToPower(3,2)  // -> 9 ( = 3 * 3 )
+// numberToPower(2,3)  // -> 8 ( = 2 * 2 * 2 )
+// numberToPower(10,6) // -> 1000000
+
+// Note: Math.pow and some others Math functions are disabled on final tests.
+
+function numberToPower(number, power){
+let result = 1
+  for (i = 0; i < power; i++) {
+    result *= number
+  }
+  return result
+}
+
+console.log(numberToPower(4, 2))
+// 16
+
+console.log(numberToPower(10, 4));
+// 10000
+
+console.log(numberToPower(10, 0));
+// 1
