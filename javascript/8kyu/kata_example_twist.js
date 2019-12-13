9f3c0103aa7d749a0641a7ba850f9b07da13c9e5
Carlos Diaz <cdiaz86@gmail.com>
2019-12-13 11:08:59 -0500

    Adds solution to 1000 array kata.

---
+++ javascript/8kyu/kata_example_twist.js
@@ -0,0 +1,8 @@
+// add the value "codewars" to the websites array 1,000 times
+var websites = []
+
+for (let index = 0; index < 1000; index++) {
+  websites.push("codewars")  
+}
+
+console.log(websites.length);
