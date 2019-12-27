980e562d5b531b0760ea4eeb02d491e6d556cdba
Carlos Diaz <cdiaz86@gmail.com>
2019-12-27 10:40:20 -0500

    Adds solution to Get Middle Character kata.

---
+++ javascript/7kyu/getMiddleCharacter.js
@@ -0,0 +1,37 @@
+// You are going to be given a word. Your job is to return the middle character of the word.
+// If the word's length is odd, return the middle character. If the word's length is even,
+// return the middle 2 characters.
+
+// #Input
+
+// A word (string) of length 0 < str < 1000 (In javascript you may get slightly more than 1000
+//   in some test cases due to an error in the test cases). You do not need to test for this.
+//   This is only here to tell you that you do not need to worry about your solution timing out.
+
+// #Output
+
+// The middle character(s) of the word represented as a string.
+
+
+function getMiddle(s) {
+  const length = s.split('').length;
+
+  if (length % 2 === 0) {
+    return s.split('').slice(((length / 2) - 1), (length / 2) + 1).join('');
+  } else {
+    return s.charAt(length / 2);
+  }
+}
+
+
+console.log(getMiddle('test'));
+// "es"
+
+console.log(getMiddle("testing"));
+// "t"
+
+console.log(getMiddle("middle"));
+// "dd"
+
+console.log(getMiddle("A"));
+// "A"
