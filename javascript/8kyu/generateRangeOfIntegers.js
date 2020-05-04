672a7f4f6c4c88ebf1bfe288398a14df3bdf33d3
Carlos Diaz <cdiaz86@gmail.com>
2020-05-04 17:35:49 -0400

    Add solution to Generate a Range of Integers kata.

---
+++ javascript/8kyu/generateRangeOfIntegers.js
@@ -0,0 +1,33 @@
+// Implement a function named generateRange(min, max, step), which takes three arguments
+// and generates a range of integers from min to max, with the step. The first integer
+// is the minimum value, the second is the maximum of the range and the third is the step. 
+// (min < max)
+
+// Implement a function named
+
+// generateRange(2, 10, 2) // should return array of [2,4,6,8,10]
+// generateRange(1, 10, 3) // should return array of [1,4,7,10]
+
+// Note
+
+//     min < max
+//     step > 0
+//     the range does not HAVE to include max (depending on the step)
+
+function generateRange(min, max, step){
+  const range = [min];
+
+  for (let index = min; index <= max; index += 1) {
+    if (index === range[range.length - 1] + step) {
+      range.push(index);
+    }
+  }
+
+  return range;
+}
+
+console.log(generateRange(2, 10, 2));
+// [2,4,6,8,10]
+
+console.log(generateRange(1, 10, 3));
+// [1,4,7,10]
