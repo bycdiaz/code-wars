0783edff24c50d5f4f6bb4bfc1edf7057ff60ece
Carlos Diaz <cdiaz86@gmail.com>
2020-05-29 18:04:41 -0400

    Adds solution to Duplicate Arguments kata.

---
+++ javascript/6kyu/duplicateArguments.js
@@ -0,0 +1,23 @@
+// Complete the solution so that it returns true if it contains any
+// duplicate argument values. Any number of arguments may be passed
+// into the function.
+
+// The array values passed in will only be strings or numbers. The
+// only valid return values are true and false.
+
+function solution(...args){
+  const count = {};
+  const moreThanOne = (value) => value > 1;
+  
+  args.forEach(arg => {count[arg] ? count[arg] += 1 : count[arg] = 1})
+  return Object.values(count).some(moreThanOne);
+ }
+
+console.log(solution(1, 2, 3));
+// false
+
+console.log(solution(1, 2, 3, 2));
+// true
+
+console.log(solution('1', '2', '3', '2'));
+// true
