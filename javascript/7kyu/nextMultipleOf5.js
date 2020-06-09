fe16b3be16a6654b70d7519769984134c385a7a8
Carlos Diaz <cdiaz86@gmail.com>
2020-06-09 08:54:22 -0400

    Adds javascript solution to Round to Next Multiple of 5 kata.

---
+++ javascript/7kyu/nextMultipleOf5.js
@@ -0,0 +1,43 @@
+// Given an integer as input, can you round it to the next (meaning, "higher") 5?
+
+// Examples:
+
+// input:    output:
+// 0    ->   0
+// 2    ->   5
+// 3    ->   5
+// 12   ->   15
+// 21   ->   25
+// 30   ->   30
+// -2   ->   0
+// -5   ->   -5
+// etc.
+
+// Input may be any positive or negative integer (including 0).
+
+// You can assume that all inputs are valid integers.
+
+function roundToNext5(n){
+  function loopUntilTarget(n) {
+    for (let i = n; i < n + 5; i += 1) {
+      if (i % 5 == 0) return i;
+    }
+  }
+
+  return n % 5 == 0 ? n : loopUntilTarget(n);
+}
+
+console.log(roundToNext5(0));
+// 0
+
+console.log(roundToNext5(2));
+// 5
+
+console.log(roundToNext5(3));
+// 5
+
+console.log(roundToNext5(-5));
+// -5
+
+console.log(roundToNext5(-2));
+// 0
