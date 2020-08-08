71cc4e6cf36ea54cad11dc255661aa995483bb28
Carlos Diaz <cdiaz86@gmail.com>
2020-08-08 10:18:11 -0400

    Refactors Multiples of 3 or 5 kata.

--- javascript/6kyu/multiples3Or5.js
+++ javascript/6kyu/multiples3Or5.js
@@ -6,19 +6,30 @@
 
 // Note: If the number is a multiple of both 3 and 5, only count it once.
 
-function solution(number) {
-  const multiples3or5 = [];
-  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+// initial solution
+// function solution(number) {
+//   const multiples3or5 = [];
+//   const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
-  for (let i = 0; i < number; i += 1) {
-    const multipleOf3 = i % 3 === 0;
-    const multipleOf5 = i % 5 === 0;
+//   for (let i = 0; i < number; i += 1) {
+//     const multipleOf3 = i % 3 === 0;
+//     const multipleOf5 = i % 5 === 0;
 
-    if (multipleOf3 || multipleOf5) {
-      multiples3or5.push(i);
-    }
-  }
-  return multiples3or5.reduce(reducer, 0);
+//     if (multipleOf3 || multipleOf5) {
+//       multiples3or5.push(i);
+//     }
+//   }
+//   return multiples3or5.reduce(reducer, 0);
+// }
+
+// alternative solution
+const multipleOfThreeOrFive = (number) => number % 3 === 0 || number % 5 === 0;
+const sumOfRange = (acc, cur) => acc + cur;
+
+function sumOfThreeOrFives(number) {
+  const initialRange = [...Array(Math.abs(number)).keys()];
+
+  return initialRange.filter(multipleOfThreeOrFive).reduce(sumOfRange, 0);
 }
 
-console.log(solution(10));
+console.log(sumOfThreeOrFives(10));
