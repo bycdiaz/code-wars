c73afd545132b85205bcae8b536606f288aa6258
Carlos Diaz <cdiaz86@gmail.com>
2020-09-25 22:18:11 -0400

    Adds JavaScript solution to Count Characters kata.

---
+++ javascript/6kyu/countCharacters.js
@@ -0,0 +1,17 @@
+// The main idea is to count all the occurring characters in a string. If you have a string like aba,
+// then the result should be {'a': 2, 'b': 1}.
+
+// What if the string is empty? Then the result should be empty object literal, {}.
+
+function count(string) {  
+  return string.split('').reduce((count, value) => {
+    count[value] ? count[value] += 1 : count[value] = 1;
+    return count;
+  }, {})
+}
+
+console.log(count('aba'));
+// { a: 2, b: 1 }
+
+console.log(count(''));
+// {}
