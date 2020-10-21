880eae51df5420324f5fb07d3c26f8a4343ba62e
Carlos Diaz <cdiaz86@gmail.com>
2020-10-21 13:42:58 -0400

    Adds 2nd attempt to Only Duplicates kata.

--- javascript/6kyu/onlyDuplicates.js
+++ javascript/6kyu/onlyDuplicates.js
@@ -1,22 +1,40 @@
 // Given a string, remove any characters that are unique from the string.
 
-function onlyDuplicates(str) {
-  const count = {};
-  let unique = [];
-  let result = '';
-  str.split('').forEach(char => {
-    count[char] ? count[char] += 1 : count[char] = 1;
-  })
+// initial attempt
+// function onlyDuplicates(str) {
+//   const count = {};
+//   let unique = [];
+//   let result = '';
+//   str.split('').forEach(char => {
+//     count[char] ? count[char] += 1 : count[char] = 1;
+//   })
 
-  for (let [key, value] of Object.entries(count)) {
-    if (value === 1) unique.push(key)
-  }
+//   for (let [key, value] of Object.entries(count)) {
+//     if (value === 1) unique.push(key)
+//   }
   
-  str.split('').forEach(char => {
-    if (!unique.includes(char)) result += char;
-  })
+//   str.split('').forEach(char => {
+//     if (!unique.includes(char)) result += char;
+//   })
 
-  return result;
+//   return result;
+// }
+
+function countLetters(stringArray) {
+  return stringArray.reduce((count, letter) => {
+    count[letter] ? count[letter]++ : count[letter] = 1;
+    return count;
+  }, {});
+}
+
+function onlyDuplicates(string) {
+  const stringAsArray = string.split('');
+  const letterCount = countLetters(stringAsArray);
+
+  return stringAsArray.reduce((string, letter) => {
+    if (letterCount[letter] > 1) string += letter;
+    return string;
+  }, '');
 }
 
 console.log(onlyDuplicates("abccdefee"));
