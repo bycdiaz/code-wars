cc7e5b1db1270d6ade9a53cab5f730d1a440ad6a
Carlos Diaz <cdiaz86@gmail.com>
2020-10-24 21:17:36 -0400

    Adds solutions to Find Odd Int kata.

--- javascript/6kyu/findOddInt.js
+++ javascript/6kyu/findOddInt.js
@@ -1,27 +1,25 @@
-// Given an array, find the integer that appears an odd number of times.
+// Given an array of integers, find the one that appears an odd number of times.
 
-// There will always be only one integer that appears an odd number of times.
-
+// initial solution
 function findOdd(array) {
   const count = {};
   let oddAppearance = 0;
 
-  array.forEach((number) => {
-    if (number in count) {
-      count[number] += 1;
-    } else {
-      count[number] = 1;
-    }
-  });
+  array.map((number) => (number in count ? count[number] += 1 : count[number] = 1));
 
   Object.keys(count).forEach((element) => {
-    if (count[element] % 2 === 1) {
-      oddAppearance = Number(element);
-    }
+    count[element] % 2 === 1 ? oddAppearance = Number(element) : null;
   });
 
   return oddAppearance;
 }
 
-console.log(findOdd([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5]));
-// 5
+function findOdd(array) {
+  const oddNum = new Set();
+  
+  for (num of array) {
+    oddNum.has(num) ? oddNum.delete(num) : oddNum.add(num);
+  }
+
+  return oddNum.values().next().value;
+}
